7da6d7c1c9f5dd52675b152ba2ee4108cc01592a
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-29 16:42:35 +0530

    cahnges

---
+++ AC-JS/Events in JS/learning/script.js
@@ -0,0 +1,8 @@
+// Select the button with id 'btn1'
+const button = document.querySelector('#btn1');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    // Show a prompt with the message
+    alert('hey hi hello');
+});
