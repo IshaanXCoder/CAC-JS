9c09caa29cf0ade699352527cdfd7f9f43ba6e95
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-30 23:08:15 +0530

    doms

---
+++ AC-JS/DOM/script.js
@@ -0,0 +1,13 @@
+console.log(document);
+
+console.log(document.querySelector('h1'));
+
+let x = document.querySelector('h1')
+// console.log(x);
+// x.innerHTML = "heyys"
+
+
+
+document.querySelector('h1').innerHTML = "heyaaaa "
+
+document.getElementsByClassName('ishaan').innerHTML = "hellllllll"
