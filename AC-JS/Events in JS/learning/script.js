3eb61766f6cf5c6a2fe0ea380d4e6635ead1aa1d
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-07-04 19:36:04 +0530

    4/7/24

--- AC-JS/Events in JS/learning/script.js
+++
@@ -1,8 +0,0 @@
-// Select the button with id 'btn1'
-const button = document.querySelector('#btn1');
-
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-    // Show a prompt with the message
-    alert('hey hi hello');
-});
---
+++ AC-JS/Events in JS/script.js
@@ -0,0 +1,22 @@
+// Select the button with id 'btn1'
+const button = document.querySelector('#btn1');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    // Show a prompt with the message
+    alert('hey hi hello');
+});
+
+
+const btn1 = document.querySelector('#btn1')
+function ishaanhumein(){
+    let a = prompt("enter a number ")
+    b =a*a;
+    console.log(`square of the number is ${b}`);
+}
+
+function hoverr(){
+    let a=0
+    a++
+    console.log(a);
+}
